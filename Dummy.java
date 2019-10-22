45f936441d0fd2c5389ac5dc98ac18ca2791fb9f
Zimmermann <uidp0978@contiwan.com>
2019-10-22 09:56:31 +0200

    Dummy als main

--- Dummy.java
+++ Dummy.java
@@ -7,6 +7,7 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.SensorMode;
 import lejos.robotics.RegulatedMotor;
 import lejos.utility.Delay;
+import plot_Y.MPlotY;
 
 class MConyeyerX {
 
@@ -39,61 +40,48 @@ class MConyeyerX {
 
 }
 
-class print {
-	void printLCD(String msg) {
-		LCD.drawString(msg, 1, 1);
+class ColorSensor {
+
+	EV3ColorSensor color_sensor = new EV3ColorSensor(SensorPort.S1);
+	SensorMode ambientSensorMode = color_sensor.getColorIDMode(); // RGB code
+
+	float[] sample = new float[ambientSensorMode.sampleSize()]; // create array of size of return value sensor
+
+	public ColorSensor() {
+		color_sensor.setFloodlight(false);
+		LCD.drawString("Init", 2, 2);
+		LCD.setAutoRefresh(false);
+
 	}
+
+	float[] getRGBCode() {
+		ambientSensorMode.fetchSample(sample, 0);
+		LCD.refresh();
+		LCD.clear();
+		LCD.drawString("Intensity: " + sample[0], 1, 1);
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		color_sensor.close();
+		return sample;
+	}
+
 }
 
 public class Dummy {
+	/*
+	 * static void printLCD(String msg) { LCD.drawString(msg, 1, 1); }
+	 */
 
 	public static void main(String[] args) {
-		RegulatedMotor m_conveyor = new EV3LargeRegulatedMotor(MotorPort.A);
-		RegulatedMotor m_z = new EV3LargeRegulatedMotor(MotorPort.C); // pen up down motor
-		RegulatedMotor m_y = new EV3LargeRegulatedMotor(MotorPort.B); // pen left right motor
-
-		EV3ColorSensor light_sensor = new EV3ColorSensor(SensorPort.S1);
-		light_sensor.setFloodlight(false);
-		LCD.drawString("Init", 2, 2);
-		LCD.setAutoRefresh(false);
-		SensorMode ambientSensorMode = light_sensor.getAmbientMode();
-
-		float[] sample = new float[ambientSensorMode.sampleSize()];
-
-		while (true) {
-			ambientSensorMode.fetchSample(sample, 0);
-			LCD.refresh();
-			LCD.clear();
-			LCD.drawString("Intensity: " + sample[0], 1, 1);
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			if (sample[0] < 0.2) {
-
-				m_conveyor.setSpeed(400);
-				m_y.setSpeed(50);
-				m_z.setSpeed(150);
-
-				// m_pen.rotate(120);
-				m_conveyor.rotate(70);
-
-			}
-			if (sample[0] >= 0.33) {
-				m_conveyor.rotate(-2500);
-				break;
-			}
-		} // end true
-		light_sensor.close();
-		m_conveyor.close();
-		m_z.close();
-		m_y.close();
-
-	}
-
-	public Dummy() {
+		LCD.drawString("Init", 1, 1);
+		ColorSensor ColSensor = new ColorSensor();
+		float[] x = ColSensor.getRGBCode();
+		LCD.drawString(x.toString(), 1, 1);
+		MPlotY PenY = new MPlotY();
 
 		// TODO Auto-generated method stub
 	}
