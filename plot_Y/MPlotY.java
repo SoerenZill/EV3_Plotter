c2c9f658883fb7cc471838d423ec281717408cef
Zimmermann <uidp0978@contiwan.com>
2019-10-22 10:51:03 +0200

    Help

--- plot_Y/MPlotY.java
+++ plot_Y/MPlotY.java
@@ -1,6 +1,5 @@
 package plot_Y;
 
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
@@ -10,52 +9,55 @@ import lejos.robotics.RegulatedMotor;
  */
 public class MPlotY {
 
-	// Direction pattern
-	enum Direction {
-		forward, backward
-	}
+    // Direction pattern
+    enum Direction {
+        forward,
+        backward
+    }
 
-	// Motor: Plotter axis Y
-	RegulatedMotor mPlotY;
+    //Motor: Plotter axis Y
+    RegulatedMotor mPlotY;
 
-	// Sensor: Plotter axis Y, touch sensor, detecting basic position
-	SPlotY sPlotY;
+    //Sensor: Plotter axis Y, touch sensor, detecting basic position
+    SPlotY sPlotY;
 
-	/**
-	 * Constructor: MPlotY
-	 *
-	 * 1. initialising mPlotY, sPlotY 2. go to basic position
-	 */
-	public MPlotY() {
-		mPlotY = new EV3LargeRegulatedMotor(MotorPort.A);
-		LCD.drawChar('1', 1, 1);
-		sPlotY = new SPlotY(this);
-		LCD.drawChar('2', 1, 1);
-		moveMPlotY(Direction.forward, 20); // Move to ground position
-		LCD.drawChar('3', 1, 1);
-	}
+    /**
+     * Constructor: MPlotY
+     *
+     * 1. initialising mPlotY, sPlotY
+     * 2. go to basic position
+     */
+    public MPlotY() {
+        mPlotY = new EV3LargeRegulatedMotor(MotorPort.C);
 
-	/**
-	 * Stop movement, axis Y
-	 */
-	void stopMPlotY() {
-		mPlotY.stop();
-	}
+        sPlotY = new SPlotY(this);
 
-	/**
-	 *
-	 * @param direction direction of movement, use .forward / .backward
-	 * @param speed     speed of movement, range 0..5 TODO change to proper value
-	 */
-	void moveMPlotY(Direction direction, int speed) {
+        moveMPlotY(Direction.forward, 20);  //Move to ground position
 
-		mPlotY.setSpeed(speed);
+    }
 
-		if (direction == Direction.forward) {
-			mPlotY.forward();
-		} else if (direction == Direction.backward) {
-			mPlotY.backward();
-		}
-	}
+    /**
+     * Stop movement, axis Y
+     */
+    void stopMPlotY() {
+        mPlotY.stop();
+    }
+
+
+    /**
+     *
+     * @param direction direction of movement, use .forward / .backward
+     * @param speed speed of movement, range 0..5 TODO change to proper value
+     */
+    void moveMPlotY(Direction direction, int speed) {
+
+        mPlotY.setSpeed(speed);
+
+        if (direction == Direction.forward) {
+            mPlotY.forward();
+        }else if (direction == Direction.backward){
+            mPlotY.backward();
+        }
+    }
 
 }
