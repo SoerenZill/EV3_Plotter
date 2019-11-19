e9388670bf3b6af40496822af86d2065de0ad9b1
Zimmermann <uidp0978@contiwan.com>
2019-11-19 11:47:15 +0100

    Synchro_Motor

--- Dummy.java
+++ Dummy.java
@@ -1,9 +1,16 @@
 import lejos.hardware.lcd.LCD;
+import lejos.robotics.RegulatedMotor;
 import plot_X.MPlotX;
 import plot_X.SPlotX;
 import plot_Y.MPlotY;
 
 public class Dummy {
+
+	static SPlotX ColSensor = new SPlotX();
+	static MPlotX ConvX = new MPlotX();
+	static MPlotY PenY = new MPlotY();
+
+	// static MPlotZ
 
 	static void printLCD(String msg) {
 		LCD.clear();
@@ -13,38 +20,49 @@ public class Dummy {
 	public static void main(String[] args) {
 		LCD.drawString("Init", 1, 1);
 
-		SPlotX ColSensor = new SPlotX();
-		MPlotX ConvX = new MPlotX();
-		MPlotY PenY = new MPlotY();
-
 		// EV3TouchSensor testTouch = new EV3TouchSensor(SensorPort.S2);
 		// SensorMode sensorMode = testTouch.getTouchMode();
 		// float[] testTouchResult = new float[sensorMode.sampleSize()];
 
+		// sensorMode.fetchSample(testTouchResult, 0);
+		// if (testTouchResult[0] == 0.5)
+
 		while (true) {
 			float[] rgb = ColSensor.getRGBCode();
 			if (rgb[0] >= 0.1 && rgb[1] >= 0.1 && rgb[2] >= 0.1) {
-				ConvX.move(MPlotX.Direction.forward, 70, 2000);
-				break;
-			}
-		}
+				ConvX.move(MPlotX.Direction.forward, 70, 3000); // mm in ms
 
-		while (true) {
-			// sensorMode.fetchSample(testTouchResult, 0);
-			// if (testTouchResult[0] == 0.5)
-			if (true) {
-				// PenY.move(MPlotY.Direction.backward, 50, 0);
-				ConvX.move(MPlotX.Direction.forward, 70, 1000);
+				synchro(70, 20, 5000);
+
 				try {
 					Thread.sleep(10);
 				} catch (Exception e) {
 					System.err.println(e);
 				}
+				ConvX.move(MPlotX.Direction.backward, 300, 10000);
 			}
+
 		}
 
 		// TODO Auto-generated method stub
 	}
 	// TODO Auto-generated method stub
+
+	private static void synchro(int x_mm, int y_mm, int ms) {
+
+		// double hypotenuse = Math.sqrt( (Math.pow(x_mm, 2) + Math.pow(x_mm, 2)) );
+
+		ConvX.mPlotX.synchronizeWith(new RegulatedMotor[] { PenY.mPlotY });
+		ConvX.mPlotX.startSynchronization();
+		// ConvX.mPlotX.rotate(-360, true);
+		// PenY.mPlotY.rotate(-360, true);
+
+		PenY.move(MPlotY.Direction.backward, y_mm, ms);
+		ConvX.move(MPlotX.Direction.forward, x_mm, ms);
+
+		ConvX.mPlotX.endSynchronization();
+		ConvX.mPlotX.waitComplete();
+		PenY.mPlotY.waitComplete();
+	}
 
 }
